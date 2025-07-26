80b8f15a3025b6b19b558a1419569caea98b7d7d
4jawaly.com <s.adel@4jawaly.net>
2025-07-26 06:13:10 +0300

    Update Get_Balance.rb

--- ruby/Get_Balance.rb
+++ ruby/Get_Balance.rb
@@ -3,15 +3,22 @@ require 'uri'
 require 'json'
 require 'base64'
 
-app_id = "api key"
-app_sec = "api secret"
+app_id = "API_KEY"
+app_sec = "API_SECRET"
 app_hash = Base64.strict_encode64("#{app_id}:#{app_sec}")
 
 base_url = "https://api-sms.4jawaly.com/api/v1/"
 
-query = {} # Define the query parameters here if needed
+query = {
+  "is_active" => 1,
+  "order_by" => "id",
+  "order_by_type" => "desc",
+  "page" => 1,
+  "page_size" => 10,
+  "return_collection" => 1
+}
 
-url = URI.parse(base_url + "account/area/me/packages?" + query.map { |k, v| "#{k}=#{v}" }.join("&"))
+url = URI.parse(base_url + "account/area/me/packages?" + URI.encode_www_form(query))
 
 headers = {
   "Accept" => "application/json",
@@ -38,7 +45,15 @@ rescue JSON::ParserError => e
 end
 
 if response_json['code'] == 200
-  puts "Your balance: #{response_json['total_balance']}"
+  # إذا كان فيه total_balance اطبعه
+  if response_json['total_balance']
+    puts "Your balance: #{response_json['total_balance']}"
+  elsif response_json['collection'] && !response_json['collection'].empty?
+    puts "Packages collection:"
+    puts JSON.pretty_generate(response_json['collection'])
+  else
+    puts "No balance or package data found!"
+  end
 else
-  puts response_json['message']
+  puts response_json['message'] || "Unknown error"
 end
